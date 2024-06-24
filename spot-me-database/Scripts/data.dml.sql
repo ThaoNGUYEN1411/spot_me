c0dcbdc7a10da73fe7b8bc57afef298c7225e43d
Thao NGUYEN <phuongthaolyon@gmail.com>
2024-06-24 16:55:21 +0200

    Addes DDL + DML scripts #1

---
+++ spot-me-database/Scripts/data.dml.sql
@@ -0,0 +1,5 @@
+DELETE FROM t_spots ;
+
+INSERT INTO  t_spots (spot_name, spot_lat, spot_lng, spot_desc) VALUES
+	('cinéma Cinéscope Louvain-La-Neuve', 48.862725, 2.287592, 'super cinéma'),
+	('Fifty Fitness', 48.784611, 2.328049, 'super salle de sports');
